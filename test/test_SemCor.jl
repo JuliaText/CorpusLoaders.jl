0da056b59210c2656b0f6b1b305c5a086591de4b
Lyndon White <lyndon.white@research.uwa.edu.au>
2018-06-13 12:39:32 +0800

    Not interned string type anymore

--- test/test_SemCor.jl
+++ test/test_SemCor.jl
@@ -20,13 +20,13 @@ end
 
 @testset "Index" begin
     corp = load(SemCor())
-    index = Dict{String, Vector{Vector{InternedString}}}()
+    index = Dict{String, Vector{Vector{String}}}()
     for sentence in flatten_levels(corp, (!lvls)(SemCor, :sent, :word))
         multisense_words = filter(x->x isa SenseAnnotatedWord,  sentence)
         context_words = word.(sentence)
         for target in multisense_words
             word_sense = sensekey(target)
-            uses = get!(Vector{Vector{InternedString}}, index, word_sense)
+            uses = get!(Vector{Vector{String}}, index, word_sense)
             push!(uses, context_words)
         end
     end
