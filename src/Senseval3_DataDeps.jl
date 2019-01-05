ea5b214295f56e08868204937a839f956525327c
Ayushk4 <ayushk4@gmail.com>
2019-01-06 00:22:41 +0530

    Modify Registration block

--- src/Senseval3_DataDeps.jl
+++ src/Senseval3_DataDeps.jl
@@ -10,7 +10,9 @@ register(DataDep("Senseval 3",
     Senseval-3 took place in March-April 2004, followed by a workshop held in July 2004 in Barcelona, in conjunction with ACL 2004.
     This is corpus was updated by Rada Mihalcea with the word senses according to WordNet 1.7.1, 2.0, 2.1, 3.0.
 
-    The details of the task in the following paper, please cite the following publication if you are using the corpora.
+    The details of the task are in the following paper, please cite the following publication if you are using the corpora.
+
+    Mihalcea, Rada, Timothy Chklovski, and Adam Kilgarriff. "The Senseval-3 English lexical sample task." In Proceedings of SENSEVAL-3, the third international workshop on the evaluation of systems for the semantic analysis of text. 2004.
     http://www.aclweb.org/anthology/W04-0807
     """,
     "http://web.eecs.umich.edu/~mihalcea/downloads/senseval.semcor/senseval3.semcor.tar.gz",
