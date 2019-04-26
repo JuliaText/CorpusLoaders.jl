2bf693d4eca9049670ab1fdaaacfdaf259087ca8
Yash Patel <yp270200@gmail.com>
2019-04-26 08:20:23 +0530

    removing SST-2 files

--- src/SST-2.jl
+++
@@ -1,6 +0,0 @@
-using DataDeps
-using DelimitedFiles
-include("SST-2_DataDeps.jl")
-
-sentiments_labels = readdlm(datadep"SST-2\\stanfordSentimentTreebank\\sentiment_labels.txt", '|')
-dictionary = readdlm(datadep"SST-2\\stanfordSentimentTreebank\\dictionary.txt", '|')
--- src/SST-2_DataDeps.jl
+++
@@ -1,8 +0,0 @@
-using DataDeps
-
-register(DataDep(
-"SST-2",
-"This contains the Stanford Senitment binary dataset",
-"http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip",
-post_fetch_method=unpack
-))
