c3bb93fb27afdedcd1be1038731054bfba4df2ca
tejasvaidhyadev <iamtejasvaidhya@gmail.com>
2020-03-15 00:40:26 +0530

    updated GMB.jl

--- src/GMB.jl
+++ src/GMB.jl
@@ -4,11 +4,11 @@ end
 
 function GMB(dirpath)
     @assert(isdir(dirpath), dirpath)
-    paths=glob("data/*/*/en.tags",dirpath)
+    paths = glob("data/*/*/en.tags",dirpath)
     GMB(paths)
 end
 
-GMB() = GMB(datadep"GMB 2.2.0") 
+GMB() = GMB(datadep"GMB 2.2.0")
 
 MultiResolutionIterators.levelname_map(::Type{GMB}) = [
     :doc=>1, :contextfile=>1, :context=>1, :document=>1,
@@ -20,38 +20,38 @@ MultiResolutionIterators.levelname_map(::Type{GMB}) = [
 
 function parse_gmb_tagged_word(line::AbstractString)
     tokens_tags = split(line, '\t')
-    return NerOnlyTaggedWord(tokens_tags[2], tokens_tags[1])
+    return PosTaggedWord(tokens_tags[2], tokens_tags[1])
 end
 
 function parse_gmb(filename)
-    local sent=[]
-	sents = @NestedVector(NerOnlyTaggedWord, 2)()
+    local sent = []
+    sents = @NestedVector(PosTaggedWord, 2)()
 
     function new_sentence()
-        sent = @NestedVector(NerOnlyTaggedWord, 1)()
+        sent = @NestedVector(PosTaggedWord, 1)()
         push!(sents, sent)
     end
-    
+
 
     # words
     get_tagged(line) = push!(sent, parse_gmb_tagged_word(line))
 
     # parse
-	for line in eachline(filename)
+    for line in eachline(filename)
         if length(line) == 0
             new_sentence()
         else
             get_tagged(line)
         end
-	end
+    end
     return sents
 end
 
 function load(corpus::GMB)
-	ch=[]
-	for fn in corpus.filepath
-    	document = parse_gmb(fn)
+    ch = @NestedVector(PosTaggedWord, 2)()
+    for fn in corpus.filepath
+        document = parse_gmb(fn)
         append!(ch, document)
-	end
+    end
     return(ch)
 end
