9db1e0b6c5b31211632245fd3f202a48b2308b97
Adarsh Kumar <adarshkumar712.ak@gmail.com>
2021-05-19 11:23:31 -0700

    Update __init_, fix #43

--- src/CorpusLoaders.jl
+++ src/CorpusLoaders.jl
@@ -13,24 +13,13 @@ export load
 
 export WikiCorpus, SemCor, Senseval3, CoNLL, IMDB, Twitter, StanfordSentimentTreebank, WikiGold, CoNLL2000
 
-function __init__()
-    include(joinpath(@__DIR__, "WikiCorpus_DataDeps.jl"))
-    include(joinpath(@__DIR__, "SemCor_DataDeps.jl"))
-    include(joinpath(@__DIR__, "SemEval2007Task7_DataDeps.jl"))
-    include(joinpath(@__DIR__, "Senseval3_DataDeps.jl"))
-    include(joinpath(@__DIR__, "CoNLL_DataDeps.jl"))
-    include(joinpath(@__DIR__, "IMDB_DataDeps.jl"))
-    include(joinpath(@__DIR__, "Twitter_DataDeps.jl"))
-    include(joinpath(@__DIR__, "StanfordSentimentTreebank_DataDeps.jl"))
-    include(joinpath(@__DIR__, "WikiGold_DataDeps.jl"))
-    include(joinpath(@__DIR__, "CoNLL2000_DataDeps.jl"))
-end
 
 include("types.jl")
 include("apply_subparsers.jl")
 include("WikiCorpus.jl")
 include("SemCor.jl")
 include("SemEval2007Task7.jl")
+include("SemEval2007Task7_DataDeps.jl")
 include("Senseval3.jl")
 include("CoNLL.jl")
 include("IMDB.jl")
@@ -38,5 +27,18 @@ include("Twitter.jl")
 include("StanfordSentimentTreebank.jl")
 include("WikiGold.jl")
 include("CoNLL2000.jl")
+
+function __init__()
+    init_datadeps(WikiCorpus)
+    init_datadeps(SemCor)
+    init_datadeps(Senseval3)
+    init_datadeps(CoNLL)
+    init_datadeps(IMDB)
+    init_datadeps(Twitter)
+    init_datadeps(StanfordSentimentTreebank)
+    init_datadeps(WikiGold)
+    init_datadeps(CoNLL2000)
+    init_datadeps_SemEval2007Task7()
+end
 
 end
--- src/Twitter.jl
+++ src/Twitter.jl
@@ -2,6 +2,27 @@ struct Twitter{S}
     data::Array{S, 1}   #array of all tweets with specified category
 end
 
+function init_datadeps(::Type{Twitter})
+    register(DataDep(
+        "Twitter Sentiment Dataset",
+        """
+        Twitter Sentiment Corpus by Niek Sanders
+        This is downloaded from:
+        http://help.sentiment140.com/for-students
+
+        This dataset contains Twitter tweets:
+        1600000 training examples
+        498 test examples
+
+        If you are using this data, cite Sentiment140
+        https://cs.stanford.edu/people/alecmgo/papers/TwitterDistantSupervision09.pdf
+        """,
+        "https://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip",
+        "004a3772c8a7ff9bbfeb875880f47f0679d93fc63e5cf9cff72d54a8a6162e57",
+        post_fetch_method=unpack
+    ))
+end
+
 function Twitter(category="train_pos")
     file_map = Dict("train" => "training.1600000.processed.noemoticon.csv", "test" => "testdata.manual.2009.06.14.csv")
     polarity_map = Dict("pos" => 4, "neg" => 0, "neu" => 2)
